11d11089d150ce9202f1616e92237fcbde95bced
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-10 12:15:13 +1300

    adding index.js and code

---
+++ index.js
@@ -0,0 +1,11 @@
+//Write a block of code that takes numbers and console log the sum of its cubes.
+let power = 3;
+let cube1 = 1**power;
+let cube2 = 5**power;
+let cube3 = 9**power;
+
+console.log(cube1 + cube2 + cube3);
+
+//you can also do this in one line for fun
+
+console.log((1**3)+(5**3)+(9**3));
