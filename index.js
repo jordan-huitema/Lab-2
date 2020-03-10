8fa23a7ebd6286184578666d252b07055cc4c8c6
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-10 18:01:11 +1300

    Adding more comments to index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Write a block of code that takes numbers and console log the sum of its cubes.
-let power = 3;
-let cube1 = 1**power;
-let cube2 = 5**power;
-let cube3 = 9**power;
+let power = 3;          //creating and asigning the power var, I have made this a variable so it can be easily changed
+let cube1 = 1**power;   //creaating and calculating the 1st cubed var
+let cube2 = 5**power;   //2nd cubed var
+let cube3 = 9**power;   //3rd cubed var
 
-console.log(cube1 + cube2 + cube3);
+console.log(cube1 + cube2 + cube3); //adding and printing the total sum of the 3 cubed var's
 
 //you can also do this in one line for fun
 
-console.log((1**3)+(5**3)+(9**3));
+console.log((1**3)+(5**3)+(9**3));  //Brackets are needed so the cubed numbers are cubed and THEN added
